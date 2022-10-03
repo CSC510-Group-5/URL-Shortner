e616a0ec3591db100ab7bc1bf35bc8bb01ea684c
Pritesh <psurana@ncsu.edu>
2022-10-03 12:18:31 -0400

    Updating update view

--- url_shortner_server/shortner/update_view.py
+++ url_shortner_server/shortner/update_view.py
@@ -1,4 +1,4 @@
-"""new_view module defines the NewView view"""
+"""update_view module defines the UpdateView view"""
 
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
@@ -9,11 +9,13 @@ class UpdateView(View):
 
     def put(self, request: HttpRequest):
         """put handles updating requests"""
-        long_url = request.PUT["long_url"]
-        link = Link(long_url)
-        link.save()
+        new_long_url = request.PUT["new_long_url"]
+        special_code = request.PUT["special_code"]
+        stub = request.PUT["stub"]
         try:
             link = Link.objects.get(stub=stub) #pylint: disable=no-member
-            return HttpResponse(status=201)
+            link.long_url = new_long_url
+            link.save()
+            return HttpResponse(link.long_url, status=201)
         except Link.DoesNotExist: #pylint: disable=no-member
             return HttpResponse(status=404)
--- url_shortner_server/shortner/views.py
+++ url_shortner_server/shortner/views.py
@@ -2,5 +2,7 @@
 
 from shortner.new_view import NewView
 from shortner.stub_view import StubView
+from shortner.update_view import UpdateView
 
-__all__ = ["NewView", "StubView"]
+
+__all__ = ["NewView", "StubView", "UpdateView"]
