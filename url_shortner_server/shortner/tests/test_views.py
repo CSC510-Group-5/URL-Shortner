8a0bf20c10f63a45e7b8ac1bc32d9368b7b729a7
Pritesh <psurana@ncsu.edu>
2022-10-06 03:09:12 -0400

    adding more test cases

--- url_shortner_server/shortner/tests/test_views.py
+++ url_shortner_server/shortner/tests/test_views.py
@@ -38,16 +38,29 @@ class TestViews(TestCase):
         }, content_type="application/json")
         self.assertEquals(update_response.status_code, 201)
 
+        # Verify invalid put request with incorrect stub
+        update_response = self.client.put(self.update_url, {
+            'new_long_url': new_long_url,
+            'special_code': special_code,
+            'stub': stub+"0"
+        }, content_type="application/json")
+        self.assertEquals(update_response.status_code, 404)
+
         self.stub_url = reverse('stub', args=[stub])
         http_response = self.client.get(self.stub_url)
         print(http_response)
         self.assertEquals(http_response.status_code, 302)
         self.assertEquals(http_response.url, new_long_url)
-
+        # Valid delete
         delete_reponse = self.client.delete(self.delete_url,{
             'special_code':special_code
         }, content_type="application/json")
         self.assertEquals(delete_reponse.status_code, 204)
+        # Verify error in deleting deleted entry
+        delete_reponse = self.client.delete(self.delete_url,{
+            'special_code':special_code
+        }, content_type="application/json")
+        self.assertEquals(delete_reponse.status_code, 404)
 
         self.stub_url = reverse('stub', args=[stub])
         http_response = self.client.get(self.stub_url)
