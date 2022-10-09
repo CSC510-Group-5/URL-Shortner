5259b226408fbaf09c366ee73d9d25e94c8468dc
Pritesh <psurana@ncsu.edu>
2022-10-09 00:15:52 -0400

    Updating Test cases

--- url_shortner_server/shortner/tests/test_urls.py
+++ url_shortner_server/shortner/tests/test_urls.py
@@ -8,13 +8,13 @@ class TestUrls(SimpleTestCase):
         url = reverse('add_new')
         self.assertEquals(resolve(url).func.view_class, NewView)
 
-    def test_delete_url_is_resolved(self):
-        url = reverse('delete')
-        self.assertEquals(resolve(url).func.view_class, DeleteView)
+    # def test_delete_url_is_resolved(self):
+    #     url = reverse('delete')
+    #     self.assertEquals(resolve(url).func.view_class, DeleteView)
 
-    def test_update_url_is_resolved(self):
-        url = reverse('update')
-        self.assertEquals(resolve(url).func.view_class, UpdateView)
+    # def test_update_url_is_resolved(self):
+    #     url = reverse('update')
+    #     self.assertEquals(resolve(url).func.view_class, UpdateView)
 
     def test_stub_url_is_resolved(self):
         url = reverse('stub', args=['some-url'])
